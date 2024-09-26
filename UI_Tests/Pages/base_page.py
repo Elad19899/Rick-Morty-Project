f17e7ba8381df20ee237078bb3a917cb6f1ee747
Elad Yerushalmi <43311896+Elad19899@users.noreply.github.com>
2024-09-26 21:12:43 +0300

    Update base_page.py

--- UI_Tests/Pages/base_page.py
+++ UI_Tests/Pages/base_page.py
@@ -8,16 +8,21 @@ class BasePage:
     def __init__(self, driver):
         self.driver = driver
 
-    def wait_for_element(self, by, value, timeout=20):  # Increased timeout for stability
-        """Wait for an element to be present on the page."""
+    def wait_for_element(self, by, value, timeout=20):
+        """
+        Waits for an element to be present on the page.
+        Args:
+            by (selenium.webdriver.common.by.By): The type of locator.
+            value (str): The value of the locator.
+            timeout (int): How long to wait for the element.
+        """
         return WebDriverWait(self.driver, timeout).until(EC.presence_of_element_located((by, value)))
 
     def wait_for_element_to_be_clickable(self, by, value, timeout=20):
-        """Wait for an element to be clickable on the page."""
+        """
+        Waits for an element to be clickable.
+        """
         return WebDriverWait(self.driver, timeout).until(EC.element_to_be_clickable((by, value)))
-
-
-
 
 
 
