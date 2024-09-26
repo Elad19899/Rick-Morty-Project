0e993ce9a3182990fb1c9103a07a28dfcb948360
Elad Yerushalmi <43311896+Elad19899@users.noreply.github.com>
2024-09-26 21:13:39 +0300

    Update google_image_search_page.py

--- UI_Tests/Pages/google_image_search_page.py
+++ UI_Tests/Pages/google_image_search_page.py
@@ -1,13 +1,18 @@
+# google_image_search_page.py
+
 import os
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from UI_Tests.Pages.base_page import BasePage
 
-class GoogleImagesPage:
-    def __init__(self, driver):
-        self.driver = driver
-
+class GoogleImagesPage(BasePage):
     def capture_screenshot(self, filename):
+        """
+        Captures a screenshot and saves it to the specified file.
+        Args:
+            filename (str): Path where the screenshot should be saved.
+        """
         # Ensure the screenshots directory exists
         if not os.path.exists('screenshots'):
             os.makedirs('screenshots')
@@ -15,13 +20,12 @@ class GoogleImagesPage:
         self.driver.save_screenshot(filename)
 
     def wait_for_images_to_load(self):
-        # Wait for images to load (optional step, you can adjust based on page structure)
+        """
+        Waits for the images to load on the search results page.
+        """
         WebDriverWait(self.driver, 10).until(
-            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'div[data-q]'))
+            EC.presence_of_all_elements_located((By.CSS_SELECTOR, 'img'))
         )
-
-
-
 
 
 
